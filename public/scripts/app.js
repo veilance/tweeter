5992a4c011078ccb834b69808a4b5511722eb091
Christopher <christopher.tam@alumni.ubc.ca>
2018-08-15 21:55:25 +0000

    form now submits a post req: 1. tweet is rendered to page async without refresh, 2. tweets are rendered from /tweets, 3. fixed security issues

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,63 +1,52 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
-
+"use strict";
 
 $(function () {
-  renderTweets(data); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+  // Object -> Element containing HTML
+  function createTweetElement(tweetObject) {
+    let $tweety = $("<article>").addClass("tweet");
+
+    let $img = $(`<img src=${tweetObject['user']['avatars']['small']}>`),
+      h1 = $(`<h1 class="name">${tweetObject['user']['name']}</h1>`),
+      h4 = $(`<h4 class="handle">${tweetObject['user']['handle']}</h4>`);
+
+    let $header = $("<header>").append($img, h1, h4);
+
+    let $body = $("<div>").addClass("text");
+    let $para = $("<p>").text(`${tweetObject['content']['text']}`);
+    $body.append($para);
+
+    let $footer = $("<footer>");
+    let $p = $(`<p>${tweetObject['created_at']}<p>`),
+      icons = $(`<div class="icons"><i class="fas fa-flag"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="fas fa-heart"></i></div>`);
+
+    $footer.append($p, icons);
+
+    $tweety.append($header);
+    $tweety.append($body);
+    $tweety.append($footer);
+    return $tweety;
+  }
+
+  function renderTweets(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (let i in tweets) {
+      $('#tweet-container').append(createTweetElement(tweets[i]));
+    }
+    return $('#tweet-container');
+  }
+
+  function loadTweets() {
+    $.get('/tweets').done(function(tweets) {
+      renderTweets(tweets);
+    })
+  }
+
+  loadTweets();
 
   // refactor later for a function to take in the parameters 
   $('article.tweet').hover(
@@ -70,42 +59,31 @@ $(function () {
       $(this).children('header').css('background', '#F0F0F0');
       $(this).find('div.icons').css('display', 'none');
     });
+
+
+  $('form#render-tweet').on('submit', function (e) {
+    e.preventDefault();
+    let data = $(this).serialize();
+
+    if ((data.length - 5) === 0) {
+      e.preventDefault();
+      alert('error: missing input');
+      return;
+    } else if ((data.length - 5) > 140) {
+      e.preventDefault();
+      alert('error: over 140');
+      return;
+    } else {
+      $.post("/tweets/", data).done(function() {
+        $('#tweet-container').empty();
+        loadTweets();
+        $('form#render-tweet textarea').val('');
+        $('form#render-tweet span.counter').text('140');
+      })
+    }
+  })
 });
 
-// Object -> Element containing HTML
-function createTweetElement(tweetObject) {
-  let $tweety = $("<article>").addClass("tweet");
 
-  let $img = $(`<img src=${tweetObject['user']['avatars']['small']}>`),
-    h1 = $(`<h1 class="name">${tweetObject['user']['name']}</h1>`),
-    h4 = $(`<h4 class="handle">${tweetObject['user']['handle']}</h4>`);
-
-  let $header = $("<header>").append($img, h1, h4);
-
-  let $body = $("<div>").addClass("text");
-  $body.append($(`<p>${tweetObject['content']['text']}</p>`));
-
-  let $footer = $("<footer>");
-  let $p = $(`<p>${tweetObject['created_at']}<p>`),
-    icons = $(`<div class="icons"><i class="fas fa-flag"></i>
-    <i class="fas fa-retweet"></i>
-    <i class="fas fa-heart"></i></div>`);
-
-  $footer.append($p, icons);
-  $tweety.append($header);
-  $tweety.append($body);
-  $tweety.append($footer);
-  return $tweety;
-}
-
-function renderTweets(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  for (let i in tweets) {
-    $('#tweet-container').append(createTweetElement(tweets[i]));
-  }
-  return $('#tweet-container');
-}
 
 
