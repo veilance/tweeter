18e7abf7145ae0eed1e27d1589857e690d7d93c5
Christopher <christopher.tam@alumni.ubc.ca>
2018-08-18 00:36:16 +0000

    Fix mongo connection string issue

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 "use strict";
 
+require('dotenv').config();
 const PORT = 8080;
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const { MongoClient } = require("mongodb");
-const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
